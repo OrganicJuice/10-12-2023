1dad53dd5cca2148dda36378eec7646f5d524e48
OrganicJuice <yding9@nd.edu>
2023-10-12 10:08:02 -0400

    Recipe class count

--- growirish final.R
+++ growirish final.R
@@ -1,4 +1,20 @@
 # 10/12/2023 class presentation 
 
-library(tidyverse)
-library(ggplot2)
+library(dplyr)
+library(DBI)
+library(dbplyr)
+library(odbc)
+
+odbcListDrivers()
+
+con <- DBI::dbConnect(odbc(),
+                      Driver = "SQL Server",
+                      Server = "mcobsql.business.nd.edu",
+                      UID = "MSBAstudent",
+                      PWD = "SQL%database!Mendoza",
+                      Port = 3306, 
+                      Database = "RecipesExample")
+
+dbListFields(con, "Recipe_Classes")
+
+dbListFields(con, "Recipes")
